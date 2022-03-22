7ad2f809ba277d1edb383722fb22faab381a51da
DiegoRojo99 <diego99rojo@gmail.com>
2022-03-22 17:04:54 -0400

    Eliminado el login

--- app/src/main/java/com/youngdred/brujowines/ui/usuario/UsuarioFragment.java
+++ app/src/main/java/com/youngdred/brujowines/ui/usuario/UsuarioFragment.java
@@ -1,9 +1,11 @@
 package com.youngdred.brujowines.ui.usuario;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.youngdred.brujowines.MainActivity;
+import com.youngdred.brujowines.TitaniaBlancoActivity;
 import com.youngdred.brujowines.databinding.FragmentUsuarioBinding;
 
 public class UsuarioFragment extends Fragment {
@@ -26,6 +30,11 @@ public class UsuarioFragment extends Fragment {
 
         binding = FragmentUsuarioBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        Button loginButton=binding.btnLogin;
+        loginButton.setOnClickListener(view -> {
+            getActivity().startActivity(new Intent(getActivity(), MainActivity.class));
+        });
 
         final TextView textView = binding.textUsuario;
         usuarioViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
