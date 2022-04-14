8a36a0298b5f85f8936390d63f0788085e5687a6
DiegoRojo99 <diego99rojo@gmail.com>
2022-04-14 12:40:58 +0200

    Las reservas salen por tipos y se puede cambiar la reserva de cata

--- app/src/main/java/com/youngdred/brujowines/ui/reservas/ReservasFragment.java
+++ app/src/main/java/com/youngdred/brujowines/ui/reservas/ReservasFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.youngdred.brujowines.R;
 import com.youngdred.brujowines.Reserva;
 import com.youngdred.brujowines.ReservarActivity;
 import com.youngdred.brujowines.databinding.FragmentReservasBinding;
@@ -33,9 +35,11 @@ public class ReservasFragment extends Fragment {
 
     private FragmentReservasBinding binding;
 
-    private TextView reservaFecha1, reservaTipo1, reservaPersonas1;
-    private TextView reservaFecha2, reservaTipo2, reservaPersonas2;
-    private Button visitaBodegaButton;
+    private int indexCata=0, indexVisita=0;
+
+    private TextView reservaFecha1, reservaPersonas1, tipo1;
+    private TextView reservaFecha2, reservaPersonas2, tipo2;
+    private Button visitaBodegaButton, cambiarReservaCataBtn, cambiarReservaVisitaBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,17 +47,27 @@ public class ReservasFragment extends Fragment {
         binding = FragmentReservasBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        reservaFecha1 = (TextView) binding.includeReserva1.tvReservaCartaFechaInfo;
-        reservaTipo1 = (TextView) binding.includeReserva1.tvReservaCartaTipoInfo;
-        reservaPersonas1 = (TextView) binding.includeReserva1.tvReservaCartaPersonasInfo;
+        reservaFecha1 = (TextView) binding.includeReservaCata.tvReservaCartaFechaInfo;
+        reservaPersonas1 = (TextView) binding.includeReservaCata.tvReservaCartaPersonasInfo;
+        tipo1 = (TextView) binding.includeReservaCata.tvCartaReservaTipo;
+        cambiarReservaCataBtn = (Button) binding.includeReservaCata.verOtraReservaButton;
 
-        reservaFecha2 = (TextView) binding.includeReserva2.tvReservaCartaFechaInfo;
-        reservaTipo2 = (TextView) binding.includeReserva2.tvReservaCartaTipoInfo;
-        reservaPersonas2 = (TextView) binding.includeReserva2.tvReservaCartaPersonasInfo;
+        reservaFecha2 = (TextView) binding.includeReservaVisita.tvReservaCartaFechaInfo;
+        reservaPersonas2 = (TextView) binding.includeReservaVisita.tvReservaCartaPersonasInfo;
+        tipo2 = (TextView) binding.includeReservaVisita.tvCartaReservaTipo;
+        cambiarReservaVisitaBtn = (Button) binding.includeReservaVisita.verOtraReservaButton;
+
+        tipo1.setText(R.string.carta_reserva_cata);
+        tipo2.setText(R.string.carta_reserva_visita);
 
         visitaBodegaButton = binding.btnReservasReservarAhora;
         visitaBodegaButton.setOnClickListener(view ->
                 startActivity(new Intent(getActivity(), ReservarActivity.class)));
+
+
+        cambiarReservaVisitaBtn.setOnClickListener(view -> cambiarVisita());
+
+        cambiarReservaCataBtn.setOnClickListener(view -> cambiarCata());
 
         cargarReservas();
 
@@ -66,34 +80,66 @@ public class ReservasFragment extends Fragment {
         binding = null;
     }
 
-    public void actualizarReserva(int index, Reserva reserva){
-        switch (index){
-            case 0:
-                //new Locale("ES") si el default no funciona
-                Format format=new SimpleDateFormat("E dd-LL-yyyy HH:mm zzz", Locale.getDefault());
-                String fechaString1=format.format(reserva.fechaReserva);
-                reservaFecha1.setText(fechaString1);
-                String numeroPersonas1=String.valueOf(reserva.numeroPersonas);
-                reservaPersonas1.setText(numeroPersonas1);
-                if(reserva.tipo){
-                    reservaTipo1.setText("Cata de Vino");
-                }else{
-                    reservaTipo1.setText("Visita a la Bodega");
-                }
-                break;
-            case 1:
-                //new Locale("ES") si el default no funciona
-                Format format2=new SimpleDateFormat("E dd-LL-yyyy HH:mm zzz", Locale.getDefault());
-                String fechaString2=format2.format(reserva.fechaReserva);
-                reservaFecha2.setText(fechaString2);
-                String numeroPersonas2=String.valueOf(reserva.numeroPersonas);
-                reservaPersonas2.setText(numeroPersonas2);
-                if(reserva.tipo){
-                    reservaTipo2.setText("Cata de Vino");
-                }else{
-                    reservaTipo2.setText("Visita a la Bodega");
-                }
-                break;
+    public void cambiarVisita(){
+
+    }
+    public void cambiarCata(){
+
+        FirebaseUser firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("reservas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int total=0; boolean found=false;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                boolean tipo=Boolean.parseBoolean(String.valueOf(document.get("Tipo")));
+
+                                if(firebaseUser.getUid().equals(document.get("UserId")) && tipo){
+                                    if(total == indexCata&&!found){
+                                        int numeroPersonas=Integer.parseInt(String.valueOf(document.get("NumeroPersonas")));
+                                        Date fechaReserva=document.getDate("FechaReserva");
+                                        Reserva res=new Reserva(fechaReserva,numeroPersonas,tipo);
+                                        actualizarReserva(res, tipo);
+                                        indexCata++;
+                                        total++;
+                                        found=true;
+                                    }else{
+                                        total++;
+                                    }
+                                }
+
+                            }
+
+                            Log.d("TAG_DIEGO","total es "+total);
+                            Log.d("TAG_DIEGO","Index es "+indexCata);
+                            if(total==indexCata){
+                                indexCata=0;
+                            }
+                        }
+                    }
+                });
+
+    }
+
+    public void actualizarReserva(Reserva reserva, boolean tipo){
+        if (tipo) {
+            //new Locale("ES") si el default no funciona
+            Format format = new SimpleDateFormat("E dd-LL-yyyy HH:mm zzz", Locale.getDefault());
+            String fechaString1 = format.format(reserva.fechaReserva);
+            reservaFecha1.setText(fechaString1);
+            String numeroPersonas1 = String.valueOf(reserva.numeroPersonas);
+            reservaPersonas1.setText(numeroPersonas1);
+        } else {
+            //new Locale("ES") si el default no funciona
+            Format format2 = new SimpleDateFormat("E dd-LL-yyyy HH:mm zzz", Locale.getDefault());
+            String fechaString2 = format2.format(reserva.fechaReserva);
+            reservaFecha2.setText(fechaString2);
+            String numeroPersonas2 = String.valueOf(reserva.numeroPersonas);
+            reservaPersonas2.setText(numeroPersonas2);
         }
     }
 
@@ -107,15 +153,13 @@ public class ReservasFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            int index=0;
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 if(firebaseUser.getUid().equals(document.get("UserId"))){
                                     boolean tipo=Boolean.parseBoolean(String.valueOf(document.get("Tipo")));
                                     int numeroPersonas=Integer.parseInt(String.valueOf(document.get("NumeroPersonas")));
                                     Date fechaReserva=document.getDate("FechaReserva");
                                     Reserva res=new Reserva(fechaReserva,numeroPersonas,tipo);
-                                    actualizarReserva(index, res);
-                                    index++;
+                                    actualizarReserva(res, tipo);
                                 }
 
                             }
