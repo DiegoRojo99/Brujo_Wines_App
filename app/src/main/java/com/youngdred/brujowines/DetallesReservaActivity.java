66edc797998d80f97b0070c9ddc391d17b3fedae
DiegoRojo99 <diego99rojo@gmail.com>
2022-04-26 12:42:39 +0200

    Creada una actividad para ver todos los detalles de la reserva

---
+++ app/src/main/java/com/youngdred/brujowines/DetallesReservaActivity.java
@@ -0,0 +1,14 @@
+package com.youngdred.brujowines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetallesReservaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+    }
+}
