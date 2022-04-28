cbc120afeb487cae342bd19b09a9296176ca11a0
DiegoRojo99 <diego99rojo@gmail.com>
2022-04-28 12:13:42 +0200

    Editar Reserva muestra los datos de la reserva seleccionada

--- app/src/main/java/com/youngdred/brujowines/EditarActivity.java
+++ app/src/main/java/com/youngdred/brujowines/EditarActivity.java
@@ -1,21 +1,40 @@
 package com.youngdred.brujowines;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class EditarActivity extends AppCompatActivity {
 
     EditText personasEt, fechaEt, tiempoEt;
     Button editarBtn;
+    ToggleButton tipoReservaBtn;
+
+    TextView tipoReservaActual, fechaReservaActual, numeroPersonasActual;
+
+    String reservaID;
 
     private int mYear, mMonth, mDay;
     private int mHour, mMinute;
@@ -25,14 +44,68 @@ public class EditarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar);
 
+        Intent reservaIntent = getIntent();
+        reservaID = reservaIntent.getStringExtra("ReservaID");
+
+        tipoReservaActual=findViewById(R.id.tv_editar_tipo_reserva_actual_info);
+        fechaReservaActual=findViewById(R.id.tv_editar_fecha_reserva_actual_info);
+        numeroPersonasActual=findViewById(R.id.tv_editar_numero_personas_actual_info);
+
         fechaEt=findViewById(R.id.et_editar_fecha_reserva_date);
         fechaEt.setOnClickListener(v -> sacarCalendario());
+
+        personasEt=findViewById(R.id.et_editar_numero_personas);
+        tipoReservaBtn=findViewById(R.id.editar_toggleButtonCataVisita);
 
         tiempoEt=findViewById(R.id.et_editar_fecha_reserva_tiempo);
         tiempoEt.setOnClickListener(view -> sacarReloj());
 
         editarBtn=findViewById(R.id.editar_editar_btn);
         editarBtn.setOnClickListener(view -> editarReserva());
+
+        getDetallesReserva();
+    }
+
+    public void getDetallesReserva(){
+
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("reservas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                if(document.getId().equals(reservaID)){
+
+                                    boolean tipo=Boolean.parseBoolean(String.valueOf(document.get("Tipo")));
+                                    int numeroPersonas=Integer.parseInt(String.valueOf(document.get("NumeroPersonas")));
+                                    Date fechaReserva=document.getDate("FechaReserva");
+                                    Reserva res=new Reserva(fechaReserva,numeroPersonas,tipo);
+                                    actualizarReserva(document.getId(),res, tipo);
+
+                                }
+
+                            }
+                        }
+                    }
+                });
+    }
+    public void actualizarReserva(String docID,Reserva reserva, boolean tipo){
+        if (tipo) {
+            tipoReservaActual.setText(R.string.cata_de_vinos);
+        } else {
+            tipoReservaActual.setText(R.string.visita_a_la_bodega);
+        }
+        Format format = new SimpleDateFormat("E dd-LL-yyyy HH:mm zzz", Locale.getDefault());
+        String fechaString= format.format(reserva.fechaReserva);
+        fechaReservaActual.setText(fechaString);
+        String numeroPersonas = String.valueOf(reserva.numeroPersonas);
+        numeroPersonasActual.setText(numeroPersonas);
+        reservaID=docID;
+
     }
 
     public void editarReserva(){
