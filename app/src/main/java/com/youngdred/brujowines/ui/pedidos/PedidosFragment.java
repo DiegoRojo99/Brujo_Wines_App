2dea32f63fcfacd4eea3795c9309db8aa0b7ad3c
DiegoRojo99 <diego99rojo@gmail.com>
2022-06-20 12:15:32 +0200

    El pedido se realiza correctamente

--- app/src/main/java/com/youngdred/brujowines/ui/pedidos/PedidosFragment.java
+++ app/src/main/java/com/youngdred/brujowines/ui/pedidos/PedidosFragment.java
@@ -1,7 +1,9 @@
 package com.youngdred.brujowines.ui.pedidos;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -9,18 +11,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.youngdred.brujowines.MainActivity;
 import com.youngdred.brujowines.R;
+import com.youngdred.brujowines.Reserva;
+import com.youngdred.brujowines.ReservarActivity;
+import com.youngdred.brujowines.TitaniaTintoActivity;
 import com.youngdred.brujowines.databinding.FragmentPedidosBinding;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class PedidosFragment extends Fragment implements View.OnClickListener {
 
     private FragmentPedidosBinding binding;
     private ImageView blancoIV, tintoIV, rosadoIV;
     private TextView blancoTV, tintoTV, rosadoTV, precioTV;
-    private Button masBlanco, menosBlanco, masRosado, menosRosado, masTinto, menosTinto;
+    private Button masBlanco, menosBlanco, masRosado, menosRosado, masTinto, menosTinto, realizarPedidoBtn;
 
     public int unidadesBlanco=0, unidadesTinto=0, unidadesRosado=0;
 
@@ -49,12 +70,15 @@ public class PedidosFragment extends Fragment implements View.OnClickListener {
         menosTinto = binding.tiendaTinto.botonMenosVino;
         masTinto = binding.tiendaTinto.botonMasVino;
 
+        realizarPedidoBtn=binding.btnRealizarPedido;
+
         masTinto.setOnClickListener(this);
         menosTinto.setOnClickListener(this);
         masBlanco.setOnClickListener(this);
         menosBlanco.setOnClickListener(this);
         masRosado.setOnClickListener(this);
         menosRosado.setOnClickListener(this);
+        realizarPedidoBtn.setOnClickListener(this);
 
         actualizarVinos();
 
@@ -151,6 +175,41 @@ public class PedidosFragment extends Fragment implements View.OnClickListener {
             menosVino("tinto");
         }else if(view==masTinto){
             masVino("tinto");
+        }else if(view==realizarPedidoBtn){
+            realizarPedido();
         }
+    }
+
+    private void realizarPedido(){
+
+        int precioTotal=30*unidadesBlanco+40*unidadesTinto+25*unidadesRosado;
+
+        FirebaseUser usuario= FirebaseAuth.getInstance().getCurrentUser();
+        String userId=usuario.getUid();
+        Map<String,Object> pedido=new HashMap<>();
+        pedido.put("Pagado",false);
+        pedido.put("Precio",precioTotal);
+        pedido.put("UnidadesBlanco",unidadesBlanco);
+        pedido.put("UnidadesRosado",unidadesRosado);
+        pedido.put("UnidadesTinto",unidadesTinto);
+        pedido.put("UserId",userId);
+
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+        db.collection("pedidos")
+                .add(pedido)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        getActivity().startActivity(new Intent(getActivity(), MainActivity.class));
+
     }
 }
